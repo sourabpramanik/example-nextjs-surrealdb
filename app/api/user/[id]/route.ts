4f6d3e68413b39efdabc429f4f1be0bf4c18dcfa
sourabpramanik <shubpramanik241@gmail.com>
2024-01-21 00:07:04 +0530

    feat: added new api

---
+++ app/api/user/[id]/route.ts
@@ -0,0 +1,15 @@
+import { User } from "@/lib/schema";
+import { surrealDatabase } from "../../lib/surreal";
+import { NextResponse } from "next/server";
+
+export async function GET(_: Request, { params }: { params: { id: string } }) {
+
+    const response = await surrealDatabase.select<User>(
+        'user:' + params.id
+    );
+
+    return NextResponse.json({
+        success: true,
+        data: response[0],
+    });
+}
